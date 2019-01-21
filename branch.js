74f1c86a00f733a6eb97b12eeb352afa8acd0086
Honkitonkie <Kooyman.d@gmail.com>
2019-01-21 20:17:53 +0100

    test
    Test

---
+++ branch.js
@@ -0,0 +1,42 @@
+function Branch() {
+  this.finished = false;
+  this.len = 150;
+  this.x;
+  this.y;
+  this.r = 150;
+  
+  this.number = random(0.8 ,0.15);
+  this.count = 1;
+
+  this.show = function() {
+    stroke(255);
+
+  this.x =150;
+  this.y =150;
+  ellipse(this.x, this.y, this.r, this.r);
+   
+  }
+
+  this.move = function(){
+if(this.r > 0.08){
+    this.x +=1;
+    this.r *= 0.99;
+   
+}
+              //console.log(this.number);
+  }
+  
+
+  
+  this.grow = function(){
+
+  }
+      
+
+      
+  
+
+
+}//Einde Branch
+
+
---
+++ sketch.js
@@ -0,0 +1,31 @@
+// Coding Rainbow
+// Daniel Shiffman
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/fcdNSZ9IzJM
+
+var tree = [];
+
+
+function setup() {
+	canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(150, 150);
+    canvas.style('z-index', '3');
+  var root = new Branch();
+  tree[0] = root;
+  background(51);
+}
+
+
+function draw() {
+
+  for (var j = 0; j < tree.length; j++) {
+    tree[j].show();
+      tree[j].move();
+      tree[j].grow();
+       ellipse(this.x, this.y, this.r, this.r);
+    //tree[i].jitter();
+  }
+
+
+
+}
