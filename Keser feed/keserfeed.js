6bfa84f0fe4490402b68e5750be27e56e0a20d0d
Honkitonkie <Kooyman.d@gmail.com>
2019-02-22 19:40:03 +0100

    update

--- Keser feed/keserfeed.js
+++
@@ -1,7 +0,0 @@
-var api = '&key=AIzaSyBdmhE0BeIZeqh1ze_2MlkxIaE4j5dXYZM';
-var google = 'https://maps.googleapis.com/maps/api/distancematrix/json?';
-var query = 'units=kilometers&origins=document.getElementById("x"),DC&destinations=document.getElementById("plaats"),NL';
-
-var url = google+query+api;
-
-document.getElementById("test").innerHTML = url;
